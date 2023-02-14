c6b784435905e48525313927e0e0bb3c67a6c4e9
Khalid Usman <123958491+kusman83@users.noreply.github.com>
2023-02-14 16:22:06 +0000

    AuthN | #52740 - Added an extention class AddCustomAuth0Configuration and AddCustomAuthentication respectively

--- src/He.PipelineAssessment.UI/Program.cs
+++ src/He.PipelineAssessment.UI/Program.cs
@@ -10,6 +10,7 @@ using He.PipelineAssessment.Infrastructure.Repository;
 using He.PipelineAssessment.Infrastructure.Repository.StoredProcedure;
 using He.PipelineAssessment.UI;
 using He.PipelineAssessment.UI.Common.Utility;
+using He.PipelineAssessment.UI.Extensions;
 using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Commands.CreateAssessmentTool;
 using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Commands.CreateAssessmentToolWorkflow;
 using He.PipelineAssessment.UI.Features.Admin.AssessmentToolManagement.Commands.UpdateAssessmentTool;
@@ -29,41 +30,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var pipelineAssessmentConnectionString = builder.Configuration.GetConnectionString("SqlDatabase");
 
- string auth0AppClientId = builder.Configuration["Auth0Config:ClientId"];
-
- string auth0AppClientSecret = builder.Configuration["Auth0Config:ClientSecret"];
-
- string auth0Domain = builder.Configuration["Auth0Config:Domain"];
-
- string identifier = builder.Configuration["Auth0Config:Identifier"];
-
-string supportEmail = builder.Configuration["Auth0Config:SupportEmail"];
-
-var auth0Config = new Auth0Config(auth0Domain, auth0AppClientId, auth0AppClientSecret);
-
-var heIdentityConfiguration = new HeIdentityCookieConfiguration
-{
-    Domain = auth0Config.Domain,
-    ClientId = auth0Config.ClientId,
-    ClientSecret = auth0Config.ClientSecret,
-    SupportEmail = supportEmail
-};
-
-var auth0ManagementConfig = new Auth0ManagementConfig(
-                            auth0Config.Domain,
-                            auth0Config.ClientId,
-                            auth0Config.ClientSecret,
-                            identifier,
-                            "???");
-
-var env = builder.Environment;
-var mvcBuilder = builder.Services.AddControllersWithViews().AddHeIdentityCookieAuth(heIdentityConfiguration, env);
-
-builder.Services.AddSingleton<HttpClient>();
-builder.Services.ConfigureIdentityManagementService(x => x.UseAuth0(auth0Config, auth0ManagementConfig));
-
-builder.Services.ConfigureHeCookieSettings(mvcBuilder,
-    configure => { configure.WithAspNetCore().WithHeIdentity().WithApplicationInsights(); });
+builder.AddCustomAuth0Configuration();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -130,25 +97,8 @@ builder.Services.AddOptions<IdentityClientConfig>()
 
 builder.Services.AddSinglePipelineClient(builder.Configuration, builder.Environment.IsDevelopment());
 
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-
-     options.AddPolicy(AuthorizationPolicies.AssignmentToPipelineAssessorRoleRequired, policy => policy.RequireRole(AppRole.PipelineAssessor));
-
-     options.AddPolicy(AuthorizationPolicies.AssignmentToPipelineAdminRoleRequired, policy => policy.RequireRole(AppRole.PipelineAdmin));
-});
-
-builder.Services.AddControllers(config =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-                     .RequireAuthenticatedUser()
-                     .Build();
-
-    config.Filters.Add(new AuthorizeFilter(policy));
-});
+//CustomExtentionsMethods.AddCustomAuthentication(builder);
+builder.Services.AddCustomAuthentication();
 
 var app = builder.Build();
 
