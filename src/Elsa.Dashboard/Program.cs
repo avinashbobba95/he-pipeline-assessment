d17c383594143f3d1bb1b49ad18b3a5a4efbe51c
MSarwar <122261875+MSarwarDev@users.noreply.github.com>
2023-02-17 10:04:48 +0000

    elsa authenticaiton WIP

--- src/Elsa.Dashboard/Program.cs
+++ src/Elsa.Dashboard/Program.cs
@@ -5,14 +5,22 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Elsa.Dashboard.Extentions;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var elsaCustomConnectionString = builder.Configuration.GetConnectionString("ElsaCustom");
 // For Dashboard.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<NonceConfig>();
-//builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<ElsaCustomContext>());
+
+builder.Services.AddDbContext<ElsaCustomContext>(config =>
+    config.UseSqlServer(elsaCustomConnectionString,
+        x => x.MigrationsAssembly("Elsa.CustomInfrastructure")));
+
+builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<ElsaCustomContext>());
 //builder.Services.AddDataProtection().PersistKeysToDbContext<ElsaCustomContext>();
+
 
 builder.AddCustomAuth0Configuration();
 
@@ -25,17 +33,20 @@ builder.Services.AddAuthorization(options =>
 
   // Register other policies here
 });
-builder.Services.AddControllers(config =>
-{
-  var policy = new AuthorizationPolicyBuilder()
-                   .RequireAuthenticatedUser()
-                   .Build();
-  config.Filters.Add(new AuthorizeFilter(policy));
-});
+//builder.Services.AddControllers(config =>
+//{
+//  var policy = new AuthorizationPolicyBuilder()
+//                   .RequireAuthenticatedUser()
+//                   .Build();
+//  config.Filters.Add(new AuthorizeFilter(policy));
+//});
 
-
+builder.Services.AddDataProtection().PersistKeysToDbContext<ElsaCustomContext>();
 
 var app = builder.Build();
+
+
+
 
 if (!app.Environment.IsDevelopment())
 {
@@ -56,21 +67,23 @@ app
         })// For Dashboard.
 
     .UseRouting()
-    .UseDirectoryBrowser();
-//.UseEndpoints(endpoints =>
-//{
-//  // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
-//  endpoints.MapControllers();
 
-//  // For Dashboard.
-//  endpoints.MapFallbackToPage("/_Host");
-//});
+    .UseDirectoryBrowser()
+    .UseAuthentication()
+    .UseAuthorization()
+.UseEndpoints(endpoints =>
+{
+  // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+  endpoints.MapControllers();
 
-app.UseAuthentication();
-app.UseAuthorization();
+  // For Dashboard.
+  endpoints.MapFallbackToPage("/_Host");
+});
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=home}/{action=index}/{id?}");
+
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=home}/{action=index}/{id?}");
 
 app.Run();
