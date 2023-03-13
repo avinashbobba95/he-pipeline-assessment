35985de1c64c2ec69f28b9b9beaf06d1215ca163
Jonny T <90678699+Jonnythan-t@users.noreply.github.com>
2023-03-13 15:59:31 +0000

    updated sql

--- src/He.PipelineAssessment.Infrastructure/Deployment/CreateAndAssignExecutorRole.sql
+++ src/He.PipelineAssessment.Infrastructure/Deployment/CreateAndAssignExecutorRole.sql
@@ -1,13 +1,12 @@
-﻿--IF NOT EXISTS(SELECT top 1 * FROM sys.database_principals  WHERE [type] = N'R' AND [name] = 'db_executor')
---BEGIN
---	CREATE ROLE db_executor
---	GRANT EXECUTE TO db_executor
---END
+﻿IF NOT EXISTS(SELECT top 1 * FROM sys.database_principals  WHERE [type] = N'R' AND [name] = 'db_executor')
+BEGIN
+	CREATE ROLE db_executor
+	GRANT EXECUTE TO db_executor
+END
 
---IF EXISTS(SELECT TOP 1 * FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
---BEGIN
---	DECLARE @UserName as varchar(100)
---	SET @UserName = (SELECT TOP 1 [name] FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
---	EXEC sp_addrolemember 'db_executor', @UserName
---END
-SELECT TEST12346567
+IF EXISTS(SELECT TOP 1 * FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
+BEGIN
+	DECLARE @UserName as varchar(100)
+	SET @UserName = (SELECT TOP 1 [name] FROM sys.database_principals  WHERE [type] = N'E' AND [name] like 'mid-pipasmt-%-01')
+	EXEC sp_addrolemember 'db_executor', @UserName
+END
